05ea0992ec12559894eb3685c85f881349b46476
Diego Debroy <3015468780101@ingenieria.usac.edu.gt>
2025-03-21 19:13:51 -0600

    ast

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d41ffafede4e846c60a66c5838fab0ac717e453e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a83a046f30f45bf64f1c120f71ebbd1583fd728")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
